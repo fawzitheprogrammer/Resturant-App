1dcee64cc55772fdd43c19e9e259efd5688cdca6
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-03 14:46:54 +0300

    More Settings Edited

--- lib/features/menu/widgets/portion_widget.dart
+++ lib/features/menu/widgets/portion_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:stackfood_multivendor/util/dimensions.dart';
 import 'package:stackfood_multivendor/util/styles.dart';
 import 'package:flutter/material.dart';
@@ -10,31 +9,66 @@ class PortionWidget extends StatelessWidget {
   final bool hideDivider;
   final String route;
   final String? suffix;
-  const PortionWidget({super.key, required this.icon, required this.title, required this.route, this.hideDivider = false, this.suffix});
+  const PortionWidget(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.route,
+      this.hideDivider = false,
+      this.suffix});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: ()=> Get.toNamed(route),
+      onTap: () => Get.toNamed(route),
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: Dimensions.paddingSizeSmall),
+        padding: const EdgeInsets.symmetric(
+          vertical: Dimensions.paddingSizeDefault,
+          horizontal: Dimensions.paddingSizeDefault,
+        ),
+        margin:
+            const EdgeInsets.symmetric(vertical: Dimensions.paddingSizeSmall),
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor.withOpacity(0.15),
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(
+            Dimensions.radiusDefault,
+          ),
+        ),
         child: Column(children: [
           Row(children: [
-            Image.asset(icon, height: 16, width: 16),
+            Image.asset(
+              icon,
+              height: 16,
+              width: 16,
+              color: Theme.of(context).colorScheme.error,
+            ),
             const SizedBox(width: Dimensions.paddingSizeSmall),
-
-            Expanded(child: Text(title, style: robotoRegular.copyWith(fontSize: Dimensions.fontSizeDefault))),
-
-            suffix != null ? Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.error,
-                borderRadius: BorderRadius.circular(Dimensions.radiusDefault),
+            Expanded(
+              child: Text(
+                title,
+                style: robotoRegular.copyWith(
+                    fontSize: Dimensions.fontSizeDefault),
               ),
-              padding: const EdgeInsets.symmetric(vertical: Dimensions.paddingSizeExtraSmall, horizontal: Dimensions.paddingSizeSmall),
-              child: Text(suffix!, style: robotoRegular.copyWith(fontSize: Dimensions.fontSizeSmall, color: Theme.of(context).cardColor)),
-            ) : const SizedBox(),
+            ),
+            suffix != null
+                ? Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.error,
+                      borderRadius:
+                          BorderRadius.circular(Dimensions.radiusDefault),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: Dimensions.paddingSizeExtraSmall,
+                        horizontal: Dimensions.paddingSizeSmall),
+                    child: Text(suffix!,
+                        style: robotoRegular.copyWith(
+                            fontSize: Dimensions.fontSizeSmall,
+                            color: Theme.of(context).cardColor)),
+                  )
+                : const SizedBox(),
           ]),
-          hideDivider ? const SizedBox() : const Divider()
+          //hideDivider ? const SizedBox() : const Divider()
         ]),
       ),
     );
--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -199,8 +199,8 @@ class AppConstants {
   static List<LanguageModel> languages = [
     LanguageModel(imageUrl: Images.english, languageName: 'English', countryCode: 'US', languageCode: 'en'),
     LanguageModel(imageUrl: Images.arabic, languageName: 'عربى', countryCode: 'SA', languageCode: 'ar'),
-    LanguageModel(imageUrl: Images.spanish, languageName: 'Spanish', countryCode: 'ES', languageCode: 'es'),
-    LanguageModel(imageUrl: Images.bengali, languageName: 'Bengali', countryCode: 'BN', languageCode: 'bn'),
+    // LanguageModel(imageUrl: Images.spanish, languageName: 'Spanish', countryCode: 'ES', languageCode: 'es'),
+    // LanguageModel(imageUrl: Images.bengali, languageName: 'Bengali', countryCode: 'BN', languageCode: 'bn'),
   ];
 
   static List<String> joinDropdown = [
